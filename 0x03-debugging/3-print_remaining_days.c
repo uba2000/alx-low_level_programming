bd855e067d70f0c7fee83e0da622f3357815480a
Noel <ubanoel2000@gmail.com>
2022-09-15 07:51:55 -0700

    fix: Leap year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -22,7 +22,7 @@ void print_remaining_days(int month, int day, int year)
 		printf("Day of the year: %d\n", day);
 		printf("Remember days: %d\n", 366 - day);
 	}
-	else 
+	else
 	{
 		if (month == 2 && day == 60)
 		{

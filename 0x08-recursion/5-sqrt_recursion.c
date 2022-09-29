f37ac44e7f7e467a134faff7c3c28ee11db537d5
uba2000 <ubanoel2000@gmail.com>
2022-09-29 19:21:31 +0100

    task: Write a function that returns the natural square root of a number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion- Find natural square root
+ * @n: int
+ * Return: int
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (square(n, 1));
+}
+
+/**
+ * square_root - find square root
+ * @n: int to find square root
+ * @val: square root
+ * Return: int
+ */
+
+int square_root(int n, int val)
+{
+	if (val * val == n)
+	{
+		return (val);
+	}
+	else if (val * val < n)
+	{
+		return (square_root(n, val + 1));
+	}
+	else
+	{
+		return (-1);
+	}
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -14,3 +14,6 @@
 
 ## 4. Once an idea has taken hold of the brain it's almost impossible to eradicate
 > Write a function that returns the value of `x` raised to the power of `y`.
+
+## 5. Your subconscious is looking for the dreamer
+> Write a function that returns the natural square root of a number.

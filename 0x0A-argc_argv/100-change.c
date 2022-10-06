1668339bc58d2f39eef5e0e972c1646a7e76e824
uba2000 <ubanoel2000@gmail.com>
2022-10-06 01:42:59 +0100

    fix: line over 80 characters (85)

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 
 /**
- * main - Prints the minimum number of coins to make change for an amount of mmoney.
+ * main - Prints the minimum number of coins to
+ * make change for an amount of mmoney.
  * @argc: The nun=mber of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
  *

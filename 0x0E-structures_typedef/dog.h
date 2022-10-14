3ea569def11c380f300b26f9ae7afa90e518cc12
uba2000 <ubanoel2000@gmail.com>
2022-10-14 18:48:53 +0100

    task: Define a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H_FILE
+#define DOG_H_FILE
+
+/**
+ * struct dog - structure definition of a dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+#endif
